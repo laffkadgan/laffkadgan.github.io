28d6f6d5d07aaf20d61791165f329964c6f0c310
laffkadgan <52545755+laffkadgan@users.noreply.github.com>
2019-07-04 21:39:15 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function(){
+
+  $('.slider-inner, .news-slider-inner').slick({
+  	nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+  	prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+  	infinite: false
+  
+
+  });
+
+  $('select').styler();
+
+  $('.header-btn-menu').on('click', function(){
+  	$('.menu ul').slideToggle();
+  });
+
+
+
+});
